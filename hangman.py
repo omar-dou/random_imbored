3180eb0451c72dc2def8b31da22d83486fcb9793
Omar Doutriaux <strangedimension2000@gmail.com>
2022-03-20 11:13:56 -0700

    first commit

---
+++ hangman.py
@@ -0,0 +1,40 @@
+import json
+import random
+import sys
+f = open("words.json")
+words = json.load(f)
+def split(a):
+    return [char for char in a]
+a = random.choice(words)
+print(a)
+win = False
+body = 0
+b = split("-----")
+s5="""---------|\n\t 0\n\t-|-\n\t ^ """
+s4="""---------|\n\t 0\n\t-|- """
+s3="""---------|\n\t 0\n\t-| """
+s2="""---------|\n\t 0\n\t | """
+s1="""---------|\n\t 0"""
+s0="""---------|"""
+parts = [s0,s1,s2,s3,s4,s5]
+while win == False:
+    ask = input("Type in a letter.\n")
+    bad = 0
+    for index, letter in enumerate(a):
+        if letter == ask:
+            b[index] = ask
+        if letter != ask:
+            bad += 1
+    if bad == 5:
+        body += 1
+    print(parts[body])
+    if int(body) == 5:
+        print("You Lost")
+        sys.exit()
+    h = ''.join(b)
+    print(h)
+    if h == a:
+        win = True
+
+    
+    
